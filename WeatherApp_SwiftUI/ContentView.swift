b223563f3d9f6269e266a3eebae8aed3c89d2ee0
metraakladap <artpadan@gmail.com>
2025-02-03 22:36:35 +0200

    Some new design, needed refactoring

--- WeatherApp_SwiftUI/ContentView.swift
+++ WeatherApp_SwiftUI/ContentView.swift
@@ -14,14 +14,127 @@ struct ContentView: View {
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .ignoresSafeArea(.all)
-            VStack {
+            VStack(spacing: 20) {
                 Text("Kyiv, Ukraine")
                     .font(.system(size: 32, weight: .medium, design: .rounded))
                     .foregroundColor(.white)
-                    .padding(.all, 32)
-                Spacer()
+                    .padding()
                 
+                VStack(spacing: 10) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("25°C")
+                        .font(.system(
+                            size: 70,
+                            weight: .medium,
+                            design: .rounded))
+                        .foregroundColor(.white)
+                }
+                
+                HStack(spacing: 15) {
+                    VStack(spacing : 6) {
+                        Text("Mon")
+                            .font(.system(
+                                size: 24,
+                                weight: .medium,
+                                design: .rounded))
+                            .foregroundColor(.white)
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 48, height: 48)
+                        Text("25°C")
+                            .font(.system(
+                                size: 24,
+                                weight: .medium,
+                                design: .rounded))
+                            .foregroundColor(.white)
+                    }
+                    VStack(spacing : 6) {
+                        Text("Mon")
+                            .font(.system(
+                                size: 24,
+                                weight: .medium,
+                                design: .rounded))
+                            .foregroundColor(.white)
+                        Image(systemName: "sun.max.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 48, height: 48)
+                        Text("25°C")
+                            .font(.system(
+                                size: 24,
+                                weight: .medium,
+                                design: .rounded))
+                            .foregroundColor(.white)
+                    }
+                    VStack(spacing : 6) {
+                        Text("Mon")
+                            .font(.system(
+                                size: 24,
+                                weight: .medium,
+                                design: .rounded))
+                            .foregroundColor(.white)
+                        Image(systemName: "wind")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 48, height: 48)
+                        Text("25°C")
+                            .font(.system(
+                                size: 24,
+                                weight: .medium,
+                                design: .rounded))
+                            .foregroundColor(.white)
+                    }
+                    VStack(spacing : 6) {
+                        Text("Mon")
+                            .font(.system(
+                                size: 24,
+                                weight: .medium,
+                                design: .rounded))
+                            .foregroundColor(.white)
+                        Image(systemName: "sunset.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 48, height: 48)
+                        Text("25°C")
+                            .font(.system(
+                                size: 24,
+                                weight: .medium,
+                                design: .rounded))
+                            .foregroundColor(.white)
+                    }
+                    VStack(spacing : 6) {
+                        Text("Mon")
+                            .font(.system(
+                                size: 24,
+                                weight: .medium,
+                                design: .rounded))
+                            .foregroundColor(.white)
+                        Image(systemName: "moon.stars.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 48, height: 48)
+                        Text("25°C")
+                            .font(.system(
+                                size: 24,
+                                weight: .medium,
+                                design: .rounded))
+                            .foregroundColor(.white)
+                    }
+                }
+                Spacer()
             }
+            
+            
             .padding()
         }
         
