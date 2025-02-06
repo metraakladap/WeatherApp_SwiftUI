6630244c246b074ca5a21d33a532dd399b4a8221
metraakladap <artpadan@gmail.com>
2025-02-06 13:36:14 +0200

    Refactored UI code

--- WeatherApp_SwiftUI/ContentView.swift
+++ WeatherApp_SwiftUI/ContentView.swift
@@ -10,32 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
-                .ignoresSafeArea(.all)
+            BackgroudView(topColor: .blue, bottomColor: Color("lightBlue"))
             VStack(spacing: 20) {
-                Text("Kyiv, Ukraine")
-                    .font(.system(size: 32, weight: .medium, design: .rounded))
-                    .foregroundColor(.white)
-                    .padding()
+                CityTextView(cityName: "Kyiv, Ukraine")
                 
-                VStack(spacing: 10) {
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    Text("25°C")
-                        .font(.system(
-                            size: 70,
-                            weight: .medium,
-                            design: .rounded))
-                        .foregroundColor(.white)
-                }
-                .padding(.bottom, 40)
+                MainWeatherStatusView(imageName: "cloud.sun.fill",
+                                      temperature: 25)
                 
-                HStack(spacing: 15) {
+                HStack(spacing: 20) {
                     WeatherDayView(dayOfWeek: "MON",
                                    imageName: "cloud.sun.fill",
                                    temperature: 25)
@@ -58,12 +40,11 @@ struct ContentView: View {
                 Button {
                     print("tapped")
                 } label: {
-                    Text("Change Day Timer")
-                        .frame(width: 200, height: 50)
-                        .background(Color.white)
-                        .font(.system(size: 20, weight: .bold, design: .default))
-                        .cornerRadius(10)
+                    WeatherButton(title: "Change Day Timer",
+                                  textColor: .blue,
+                                  backgroundColor: .white)
                 }
+                
                 Spacer()
             }
             
@@ -107,3 +88,53 @@ struct WeatherDayView: View {
         }
     }
 }
+
+struct BackgroudView: View {
+    
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .ignoresSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .rounded))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            Text("\(temperature)°C")
+                .font(.system(
+                    size: 70,
+                    weight: .medium,
+                    design: .rounded))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 40)
+    }
+}
+
+
---
+++ WeatherApp_SwiftUI/WeatherButton.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherButton.swift
+//  WeatherApp_SwiftUI
+//
+//  Created by User on 06.02.2025.
+//
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 200, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
